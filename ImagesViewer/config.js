bc75348acd2e65cd40e25dcad578483b8413c47f
ChucNX <74488821+chucnxwork@users.noreply.github.com>
2022-11-25 21:05:59 +0700

    Add show random image feature in Image Viewer tool

--- ImagesViewer/config.js
+++ ImagesViewer/config.js
@@ -1,6 +1,13 @@
-function getAssetsArrs(id, min, max, fileExtension='j') {
+function getAssetsArrs(id, min, max, fileExtension='j', startAtRandom=false) {
 	var arrs = []
-	for (var i=min; i<=max; i++) {
+	var randomStart = min
+	if (startAtRandom) {
+		randomStart = Math.floor(Math.random() * (max - min + 1)) + min
+	}
+	for (var i=randomStart; i<=max; i++) {
+		arrs[arrs.length] = `data/${id}/1 (${i}).${fileExtension}`
+	}
+	for (var i=min; i<randomStart; i++) {
 		arrs[arrs.length] = `data/${id}/1 (${i}).${fileExtension}`
 	}
 	return arrs
@@ -10,14 +17,14 @@ const MEDIA_COLLECTIONS = [
 	{
 		id: '1',
 		items: [
-			...getAssetsArrs('1',1,2,'jpg'),
+			...getAssetsArrs('1',1,2,'jpg',true),
 			
 		]
 	},
 	{
 		id: '2',
 		items: [
-			...getAssetsArrs('2',1,2,'jpg'),
+			...getAssetsArrs('2',1,2,'jpg',true),
 			
 		]
 	},
